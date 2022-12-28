9eb307c94bb85230fcea82e9038f0cd097fbcb1d
Hill-0xFF <leadlevelprogrammer@gmail.com>
2022-12-27 22:34:08 -0500

    [dev]: Implementing the test, it should fail

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,2 +1,7 @@
 # TDD fizzbuzz.py
 # Replace numbers that are divisible by 3 with 'fizz', by 5 with 'buzz' and by 3 and 5 with 'fizzbuzz'
+def test_fizzbuzz():
+    """Implementing Fizzbuzz algorithm with TDD (Test Driven Development) in mind
+    >>> from fizzbuzz import fizzbuzz
+    
+    """
